e7200fe10048a4763f3dcd69d05119a264babe1c
masiullah <masiullah@gmail.com>
2012-07-14 19:17:57 +0530

    Stylus gestures features (2/2)
    Forward-port of http://review.cyanogenmod.com/#/c/18613/6
    to JB
    
    Also port https://github.com/CyanogenMod/android_packages_apps_Settings/commit/52be9d5454c1faee70e3e3c4ab6180dd946d1521#diff-4a720ef4ec7ced588c0a86b81baf964f and https://github.com/CyanogenMod/android_packages_apps_Settings/commit/035f81c5da26d11f2b8486ed7118d74a3ec5c1a9#diff-771f8325dc0715a45fb34ba355671712
    
    Change-Id: Iebdcc8b3381ae72c417d6cf38f3cfb47eb5dbf11

--- src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
+++ src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
@@ -60,6 +60,8 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
     private static final String KEY_CURRENT_INPUT_METHOD = "current_input_method";
     private static final String KEY_INPUT_METHOD_SELECTOR = "input_method_selector";
     private static final String KEY_USER_DICTIONARY_SETTINGS = "key_user_dictionary_settings";
+	private static final String KEY_POINTER_SETTINGS = "pointer_settings_category";
+	private static final String KEY_STYLUS_ICON_ENABLED = "stylus_icon_enabled";
     // false: on ICS or later
     private static final boolean SHOW_INPUT_METHOD_SWITCHER_SETTINGS = false;
 
@@ -71,6 +73,7 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
         "auto_replace", "auto_caps", "auto_punctuate",
     };
 
+	private CheckBoxPreference mStylusIconEnabled;
     private int mDefaultInputMethodSelectorVisibility = 0;
     private ListPreference mShowInputMethodSelectorPref;
     private PreferenceCategory mKeyboardSettingsCategory;
@@ -169,6 +172,8 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
         mIm = (InputManager)getActivity().getSystemService(Context.INPUT_SERVICE);
         updateInputDevices();
 
+		mStylusIconEnabled = (CheckBoxPreference) findPreference(KEY_STYLUS_ICON_ENABLED);
+
         // Spell Checker
         final Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.setClass(getActivity(), SpellCheckersSettingsActivity.class);
@@ -180,6 +185,13 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
 
         mHandler = new Handler();
         mSettingsObserver = new SettingsObserver(mHandler, getActivity());
+		// remove the pointer settings preference category for non stylus devices
+        if (!hasStylus()) {
+            PreferenceCategory pc = (PreferenceCategory) findPreference(KEY_POINTER_SETTINGS);
+            if (pc != null) {
+                getPreferenceScreen().removePreference(pc);
+            }
+        }
     }
 
     private void updateInputMethodSelectorSummary(int value) {
@@ -266,6 +278,11 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
             }
         }
 
+        if (mStylusIconEnabled != null) {
+            mStylusIconEnabled.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.STYLUS_ICON_ENABLED, 0) == 1);
+        }
+
         // Hard keyboard
         if (!mHardKeyboardPreferenceList.isEmpty()) {
             for (int i = 0; i < sHardKeyboardKeys.length; ++i) {
@@ -319,6 +336,9 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
         if (Utils.isMonkeyRunning()) {
             return false;
         }
+        } else if (preference == mStylusIconEnabled) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.STYLUS_ICON_ENABLED, mStylusIconEnabled.isChecked() ? 1 : 0);
         if (preference instanceof PreferenceScreen) {
             if (preference.getFragment() != null) {
                 // Fragment will be handled correctly by the super class.
@@ -570,5 +590,10 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
         public void pause() {
             mContext.getContentResolver().unregisterContentObserver(this);
         }
+    }
+
+    // returns whether the device has stylus or not
+    private boolean hasStylus() {
+        return getResources().getBoolean(com.android.internal.R.bool.config_stylusGestures);
     }
 }
