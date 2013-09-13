a3e0e858c3f849ba456857c41521873fa4b5149b
xplodwild <xplod@ouverta.fr>
2013-09-13 15:39:29 +0100

    [1/2] Settings: Add lockscreen shortcuts settings
    Change-Id: Ia38797d27ea1255a4f35f214c5c7ace126334f1c

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -817,6 +817,7 @@ public class Settings extends PreferenceActivity
     public static class UserDictionarySettingsActivity extends Settings { /* empty */ }
     public static class SoundSettingsActivity extends Settings { /* empty */ }
     public static class BarsAndMenusSettingsActivity extends Settings { /* empty */ }
+    public static class LockscreenTargetsSettingsActivity extends Settings { /* empty */ }
     public static class DisplaySettingsActivity extends Settings { /* empty */ }
     public static class DeviceInfoSettingsActivity extends Settings { /* empty */ }
     public static class ApplicationSettingsActivity extends Settings { /* empty */ }
