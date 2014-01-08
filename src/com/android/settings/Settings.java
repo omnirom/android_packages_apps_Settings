8c589e88dd2429d90c3d1f8bd30d14e160e71c59
UtkarshGupta <utkarsh.eminem@gmail.com>
2014-01-08 23:27:23 +0530

    [3/3] Settings: Toggle lock screen camera widget
    Base: https://gerrit.omnirom.org/#/c/4700/
    OmniGears: https://gerrit.omnirom.org/#/c/4701/
    
    Change-Id: Ic485195060c071f120022a52cfd85226b0c3a414

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1096,6 +1096,7 @@ public class Settings extends PreferenceActivity
     public static class NotificationPanelSettingsActivity extends Settings { /* empty */ }
     public static class MoreInterfaceSettingsActivity extends Settings { /* empty */ }
     public static class ActiveDisplaySettingsActivity extends Settings { /* empty */ }
+    public static class LockscreenSettingsActivity extends Settings { /* empty */ }
     public static class DisplaySettingsActivity extends Settings { /* empty */ }
     public static class DeviceInfoSettingsActivity extends Settings { /* empty */ }
     public static class ApplicationSettingsActivity extends Settings { /* empty */ }
