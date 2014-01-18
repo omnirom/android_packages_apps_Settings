fc3e11be66165f27e4596cc82edcea633a0d8f57
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-01-17 22:08:18 -0600

    Klozz twitter add...

--- src/com/android/settings/About.java
+++ src/com/android/settings/About.java
@@ -37,8 +37,8 @@ public class About extends SettingsPreferenceFragment {
 
     private static final String ANIME_REVIEW = "https://github.com/AnimeROM"; //Github of AnimeROM PUT HERE
     private static final String ANIME_TWITTER = "https://github.com/AnimeROM"; // twiter or any ??
-    private static final String XPERIAFAN13 = "https://www.facebook.com/miguelangel.sanchezbravo"; //here your direction
-    private static final String KLOZZ = ""; // here your direction
+    private static final String XPERIAFAN13 = "https://www.facebook.com/miguelangel.sanchezbravo"; 
+    private static final String KLOZZ = "https://twitter.com/klozz_"; 
 
     Preference mReviewUrl;
     Preference mxperiafan13;
