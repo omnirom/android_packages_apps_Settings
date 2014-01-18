f2a62f4528e9f29c71fd1b27b29b21ca8a8693a7
xperiafan13-rom <androidanimerom@gmail.com>
2014-01-17 22:03:54 -0600

    Update About.java

--- src/com/android/settings/About.java
+++ src/com/android/settings/About.java
@@ -37,7 +37,7 @@ public class About extends SettingsPreferenceFragment {
 
     private static final String ANIME_REVIEW = "https://github.com/AnimeROM"; //Github of AnimeROM PUT HERE
     private static final String ANIME_TWITTER = ""; // twiter or any ??
-    private static final String XPERIAFAN13 = ""; //here your direction
+    private static final String XPERIAFAN13 = "https://www.facebook.com/miguelangel.sanchezbravo"; //here your direction
     private static final String KLOZZ = ""; // here your direction
 
     Preference mReviewUrl;
