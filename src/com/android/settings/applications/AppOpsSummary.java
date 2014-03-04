368fbc9619172885bbf64732b5a8849f4f4c857d
David James <James.N.David@gmail.com>
2014-03-03 19:24:34 -0600

    Color Changes
    Change-Id: I404b88ded9b741f052d955847cdadb396f69c246

--- src/com/android/settings/applications/AppOpsSummary.java
+++ src/com/android/settings/applications/AppOpsSummary.java
@@ -130,7 +130,7 @@ public class AppOpsSummary extends Fragment {
         mViewPager.setAdapter(mAdapter);
         mViewPager.setOnPageChangeListener(mAdapter);
         PagerTabStrip tabs = (PagerTabStrip) rootView.findViewById(R.id.tabs);
-        tabs.setTabIndicatorColorResource(android.R.color.holo_blue_light);
+        tabs.setTabIndicatorColorResource(android.R.color.black);
 
         // We have to do this now because PreferenceFrameLayout looks at it
         // only when the view is added.
