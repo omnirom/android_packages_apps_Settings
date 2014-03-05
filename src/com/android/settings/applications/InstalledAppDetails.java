d88ebbc57a6e38350ede8fd37dae15b3ef10d301
ByteHamster <info@bytehamster.com>
2014-03-05 15:08:17 +0100

    Settings: Add AppOps link to app details
    http://jira.omnirom.org/browse/OMNI-69
    
    Change-Id: Id9269dac81a3dc5e27072bd727244b9417bcbba8

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -21,6 +21,7 @@ import com.android.internal.telephony.SmsUsageMonitor;
 import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settings.applications.ApplicationsState.AppEntry;
+import com.android.settings.applications.AppOpsDetails;
 
 import android.app.Activity;
 import android.app.ActivityManager;
@@ -140,6 +141,7 @@ public class InstalledAppDetails extends Fragment
     private Button mForceStopButton;
     private Button mClearDataButton;
     private Button mMoveAppButton;
+    private Button mAppOpsButton;
     private CompoundButton mNotificationSwitch;
 
     private PackageMoveObserver mPackageMoveObserver;
@@ -473,6 +475,7 @@ public class InstalledAppDetails extends Fragment
         mEnableCompatibilityCB = (CheckBox)view.findViewById(R.id.enable_compatibility_cb);
         
         mNotificationSwitch = (CompoundButton) view.findViewById(R.id.notification_switch);
+        mAppOpsButton = (Button) view.findViewById(R.id.app_ops_button);
 
         return view;
     }
@@ -551,6 +554,21 @@ public class InstalledAppDetails extends Fragment
         TextView packageName = (TextView) appSnippet.findViewById(R.id.app_pkgname);
         packageName.setText(mAppEntry.info.packageName);
         packageName.setVisibility(View.VISIBLE);
+        // Go to AppOps button
+        mAppOpsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle args = new Bundle();
+                args.putString(AppOpsDetails.ARG_PACKAGE_NAME, mAppEntry.info.packageName);
+
+                AppOpsDetails appOps = new AppOpsDetails();
+                appOps.setArguments(args);
+                getFragmentManager().beginTransaction()
+                .add(android.R.id.content, appOps)
+                .addToBackStack(null)
+                .commit();
+            }
+        });
         // Version number of application
         mAppVersion = (TextView) appSnippet.findViewById(R.id.app_size);
 
