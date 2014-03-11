60c8a94313f321e5736fab7d46425459c097bf19
Lars Greiss <kufikugel@googlemail.com>
2014-03-11 21:31:25 +0700

    [4/4] Settings: update Quiet Hours
    follow slimroms changes
    
    Settings: Add "Is Starred" to Quiet Hours (1/1)
    
    Starred contacts only for bypass and response
    	settings.
    
    https://gerrit.slimroms.net/#/c/5191/
    
    Settings: QuierHours service: call with qualified user and introduce controller
    
    This commit fixes 2 issues:
    
    1. Call the service with a qualified user.
    
    2. Complete overwork of the handling. Currently the SmsCallHellper is doing
       way to much work. Especially when time change broadcasts trigger the service
       update. Which ended in certain cases to a complete system overload.
       To fix it we use now a single tone controller to take care of this all
    
    https://gerrit.slimroms.net/#/c/5749/
    
    Settings: calendar is not a single tone
    
    uh man should not fix stuff on 4am morning :/
    
    as well fix a logic derp
    
    and another one :/
    
    https://gerrit.slimroms.net/#/c/5756/
    
    Settings: let the controller handle all
    
    observer should update the service.
    
    Fix ANR for schedulService() loop on start service
    	command issued
    
    PS:
    After Juba fixed the root cause and the loop the boolean check is not needed anymore
    
    https://gerrit.slimroms.net/#/c/5814/
    
    Settings: Prevent GC from grabbing observer
    https://gerrit.slimroms.net/#/c/5822/
    
    Quiet Hours: Allow disabling phone ringer
    https://github.com/CyanogenMod/android_packages_apps_Settings/commit/3971186ba4895791168cffaa27cc0fee4156648e
    
    also add notification if quiet hours enabled
    http://jira.omnirom.org/browse/OMNI-612
    
    tweak de service
    
    Change-Id: If37d3f710dc277ec75c55c2b0c71d91f90059cc7

--- src/com/android/settings/slim/service/BootReceiver.java
+++ src/com/android/settings/slim/service/BootReceiver.java
@@ -19,11 +19,12 @@ package com.android.settings.slim.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.provider.Settings;
 
 public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        SmsCallHelper.scheduleService(context);
+        SmsCallController.getInstance(context).scheduleService();
     }
 }
