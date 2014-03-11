033946f54526b7afdeba3d411c3e52534e5a79c6
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-11 21:31:25 +0700

    Settings: QuietHour: dont call services on every option changes
    if user changing QuietHour settings, this will call services to start/stop
    also will make alarmmanager to setup same intent
    
    this will cause multiple call services, and make alamrmanager
    will aquire wakelock wihout release
    
    also add notification if quiet hours enabled
    http://jira.omnirom.org/browse/OMNI-612
    
    tweak de service
    
    Change-Id: If37d3f710dc277ec75c55c2b0c71d91f90059cc7

---
+++ src/com/android/settings/slim/service/DisableQuietHour.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2014 The OmniRom Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.slim.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+
+import com.android.settings.R;
+
+public class DisableQuietHour extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        boolean checkModeOn = Settings.System.getInt(context.getContentResolver(),
+                                                Settings.System.QUIET_HOURS_ENABLED, 0) == 1;
+
+        SmsCallHelper.setQuietHoursActive(context, checkModeOn ? 0 : 1);
+        SmsCallHelper.scheduleService(context);
+    }
+
+}
