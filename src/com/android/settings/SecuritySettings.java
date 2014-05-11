3975a0c4a3c064cf0ca402e9693bd722a58e2c45
Tom Briden <tom@decompile.me.uk>
2014-05-11 14:44:24 +0100

    [1/3] Settings: Nfc Unlock
    Adds setting to security where Nfc tags can be added to a list of authorised tags that can bypass the lock screen security
    
    Requires changes to frameworks_base and packages_apps_Settings
    
    https://gerrit.omnirom.org/#/c/8991/ and https://gerrit.omnirom.org/#/c/8990/
    
    Original Link https://github.com/AospPlus/android_packages_apps_Settings/commit/a6f7f59aafef1d4f29b74355c56d341de5854f50
    
    Change-Id: I35214253c32687650ae2e2ba7dc54e9cedb10175

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -69,6 +69,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private static final String KEY_LOCK_AFTER_TIMEOUT = "lock_after_timeout";
     private static final String KEY_OWNER_INFO_SETTINGS = "owner_info_settings";
     private static final String KEY_ENABLE_WIDGETS = "keyguard_enable_widgets";
+    private static final String KEY_NFC_UNLOCK = "nfc_unlock_settings";
 
     private static final int SET_OR_CHANGE_LOCK_METHOD_REQUEST = 123;
     private static final int CONFIRM_EXISTING_FOR_BIOMETRIC_WEAK_IMPROVE_REQUEST = 124;
@@ -303,7 +304,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
 
         mQuickUnlockScreen = (CheckBoxPreference) root.findPreference(LOCKSCREEN_QUICK_UNLOCK_CONTROL);
         if (mQuickUnlockScreen  != null) {
-            mQuickUnlockScreen.setChecked(Settings.System.getInt(getContentResolver(), 
+            mQuickUnlockScreen.setChecked(Settings.System.getInt(getContentResolver(),
                     Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL, 0) == 1);
             mQuickUnlockScreen.setOnPreferenceChangeListener(this);
         }
@@ -360,6 +361,13 @@ public class SecuritySettings extends RestrictedSettingsFragment
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
             // No telephony, remove dependent options
             root.removePreference(mBlacklist);
+        }
+
+         // Determine options based on device nfc support
+         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_NFC)) {
+         // No nfc, remove dependent options
+         root.removePreference(mBlacklist);
+
         }
 
         mNotificationAccess = findPreference(KEY_NOTIFICATION_ACCESS);
@@ -615,7 +623,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
                     Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL, isToggled(preference) ? 1 : 0);
         } else if (preference == mMenuUnlock) {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                    Settings.System.MENU_UNLOCK_SCREEN, isToggled(preference) ? 1 : 0);        
+                    Settings.System.MENU_UNLOCK_SCREEN, isToggled(preference) ? 1 : 0);
         } else if (preference == mShowPassword) {
             Settings.System.putInt(getContentResolver(), Settings.System.TEXT_SHOW_PASSWORD,
                     mShowPassword.isChecked() ? 1 : 0);
@@ -629,6 +637,10 @@ public class SecuritySettings extends RestrictedSettingsFragment
         } else if (KEY_TOGGLE_VERIFY_APPLICATIONS.equals(key)) {
             Settings.Global.putInt(getContentResolver(), Settings.Global.PACKAGE_VERIFIER_ENABLE,
                     mToggleVerifyApps.isChecked() ? 1 : 0);
+        } else if (DeviceUtils.deviceSupportsNfc(mContext)) {
+            final Intent intent = new Intent();
+            intent.setClassName("com.android.settings", "com.android.settings.NfcUnlockSettings");
+            startActivity(intent);
         } else {
             // If we didn't handle it, let preferences handle it.
             return super.onPreferenceTreeClick(preferenceScreen, preference);
