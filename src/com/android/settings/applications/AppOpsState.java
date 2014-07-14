e385b6b88cdfae3686784c46a15b7e259e7f0e03
pulser <pulser@xda-developers.com>
2014-07-14 21:10:48 +0100

    Settings AppOps: Forward port wifi/bluetooth/mobile/wakeup ops
    Originally from sam3000
    
    Change-Id: Iacfbf6abccdda65afc3b7b83bfc03b30d94afd77

--- src/com/android/settings/applications/AppOpsState.java
+++ src/com/android/settings/applications/AppOpsState.java
@@ -193,13 +193,21 @@ public class AppOpsState {
                     AppOpsManager.OP_CALL_PHONE,
                     AppOpsManager.OP_WRITE_SETTINGS,
                     AppOpsManager.OP_SYSTEM_ALERT_WINDOW,
-                    AppOpsManager.OP_WAKE_LOCK },
+                    AppOpsManager.OP_WAKE_LOCK,
+                    AppOpsManager.OP_WIFI_CHANGE,
+                    AppOpsManager.OP_BLUETOOTH_CHANGE,
+                    AppOpsManager.OP_DATA_CONNECT_CHANGE,
+                    AppOpsManager.OP_ALARM_WAKEUP, },
             new boolean[] { false,
                     true,
                     true,
                     true,
                     true,
-                    true,  }
+                    true,
+                    true,
+                    true,
+                    true,
+                    true }
             );
 
     public static final OpsTemplate BOOTUP_TEMPLATE = new OpsTemplate(
