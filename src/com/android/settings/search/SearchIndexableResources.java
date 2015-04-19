346b3913515334f6e49b62dd468280dbcd4c7461
maxwen <max.weninger@gmail.com>
2015-04-20 01:26:10 +0200

    [2/3] Settings: battery styles
    Change-Id: I9ba4280381d714403baa30ae28b413fe92bf48f2

--- src/com/android/settings/search/SearchIndexableResources.java
+++ src/com/android/settings/search/SearchIndexableResources.java
@@ -53,6 +53,7 @@ import com.android.settings.wifi.WifiSettings;
 import org.omnirom.omnigears.AmbientSettings;
 import org.omnirom.omnigears.ButtonSettings;
 import org.omnirom.omnigears.batterylight.BatteryLightSettings;
+import org.omnirom.omnigears.interfacesettings.BarsSettings;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -296,6 +297,13 @@ public final class SearchIndexableResources {
                         NO_DATA_RES_ID,
                         BatteryLightSettings.class.getName(),
                         R.drawable.ic_settings_notifications));
+
+        sResMap.put(BarsSettings.class.getName(),
+                new SearchIndexableResource(
+                        Ranking.getRankForClassName(BarsSettings.class.getName()),
+                        NO_DATA_RES_ID,
+                        BarsSettings.class.getName(),
+                        R.drawable.ic_bars));
 
     }
 
