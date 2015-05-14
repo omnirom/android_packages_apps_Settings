3e55ae463945371c0b7bcdc1498dff4ecdb706b5
Nicolas Prevot <nprevot@google.com>
2015-05-13 17:19:21 -0700

    Remove the check hasMultipleUsers in Settings.
    When deciding to show the user section or not, Just rely
    on supportsMultipleUsers. Don't check hasMultipleUsers.
    
    BUG:21119929
    Change-Id: Ifbddac10e2f6dc02b8e2e55b41ab9f5be018dfca

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1204,8 +1204,7 @@ public class SettingsActivity extends Activity
                             ((UserManager) getSystemService(Context.USER_SERVICE))
                                     .getUserCount() > 1;
                     if (!UserHandle.MU_ENABLED
-                            || (!UserManager.supportsMultipleUsers()
-                                    && !hasMultipleUsers)
+                            || !UserManager.supportsMultipleUsers()
                             || Utils.isMonkeyRunning()) {
                         removeTile = true;
                     }
