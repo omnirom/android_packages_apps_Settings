f0ef3fb384e4ffe6bbfd215339c517cc327baa80
Alex Cruz <mazdarider23@gmail.com>
2016-01-21 11:06:02 -0500

    [1/2] Settings: Configurable dashboard columns
    Before this, the only way to get double columns on a device like the Nexus 6
    was to adjust the config.xml and while that works for some, it doesn't for all.
    
    This method allows those that like to adjust the columns to do so while keeping
    the stock look :-)
    
    Thanks to @xWASABI for the idea in where he adjusted the config.xml
    https://github.com/TeamTwisted/packages_apps_Settings/commit/3c62ed2f244ae21436e02556894c2f5b46365024
    
    Adjusted for Omni
    
    Change-Id: Id99ad00974ec5fe3aeef8e9cbabf6fe156c54bf2

--- src/com/android/settings/dashboard/DashboardContainerView.java
+++ src/com/android/settings/dashboard/DashboardContainerView.java
@@ -17,27 +17,43 @@
 package com.android.settings.dashboard;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import com.android.settings.R;
 
+import com.android.internal.util.omni.DeviceUtils;
+
 public class DashboardContainerView extends ViewGroup {
 
+    public static final String PREF_DASHBOARD_COLUMNS = "dashboard_columns";
     private float mCellGapX;
     private float mCellGapY;
 
     private int mNumRows;
     private int mNumColumns;
+    private boolean mCompactMode;
 
     public DashboardContainerView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         final Resources res = context.getResources();
-        mCellGapX = res.getDimension(R.dimen.dashboard_cell_gap_x);
+        final int dashboardValue = res.getInteger(R.integer.dashboard_num_columns);
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        // dont use a vale smaller then what the config said
+        // special case for layout-land which has a default of 2
+        mNumColumns = Math.max(dashboardValue, Integer.valueOf(prefs.getString(PREF_DASHBOARD_COLUMNS,
+                Integer.toString(dashboardValue))));
+        final boolean isLandsacpe = res.getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        mCompactMode = (DeviceUtils.isPhone(context) && (isLandsacpe ? mNumColumns > 2 : mNumColumns > 1))
+                || (DeviceUtils.isTablet(context) && mNumColumns > 2);
+        mCellGapX = res.getDimension(mCompactMode ? R.dimen.dashboard_cell_gap_x_compact : R.dimen.dashboard_cell_gap_x);
         mCellGapY = res.getDimension(R.dimen.dashboard_cell_gap_y);
-        mNumColumns = res.getInteger(R.integer.dashboard_num_columns);
     }
 
     @Override
@@ -138,5 +154,9 @@ public class DashboardContainerView extends ViewGroup {
                 y += childHeight + mCellGapY;
             }
         }
+    }
+
+    protected boolean isCompactMode() {
+        return mCompactMode;
     }
 }
