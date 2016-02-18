dbfe943e60fdf4783d212defc1615c8f52142b59
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2016-02-17 22:53:05 -0800

    Settings: delete duplicated cast option
    We already have the one that we should keep on Display.
    
    Change-Id: I2d5bf6a182fcf03fdf422a61dfa9bc09b7fd14d9

--- src/com/android/settings/notification/NotificationSettings.java
+++ src/com/android/settings/notification/NotificationSettings.java
@@ -80,7 +80,6 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
     private static final String KEY_NOTIFICATION_RINGTONE = "notification_ringtone";
     private static final String KEY_ALARM_RINGTONE = "alarm_ringtone";
     private static final String KEY_VIBRATE_WHEN_RINGING = "vibrate_when_ringing";
-    private static final String KEY_WIFI_DISPLAY = "wifi_display";
     private static final String KEY_NOTIFICATION = "notification";
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
     private static final String KEY_LOCK_SCREEN_NOTIFICATIONS = "lock_screen_notifications";
@@ -790,7 +789,6 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
             if (!Utils.isVoiceCapable(context)) {
                 rt.add(KEY_RING_VOLUME);
                 rt.add(KEY_PHONE_RINGTONE);
-                rt.add(KEY_WIFI_DISPLAY);
                 rt.add(KEY_VIBRATE_WHEN_RINGING);
                 rt.add(KEY_VOLUME_LINK_NOTIFICATION);
             }
