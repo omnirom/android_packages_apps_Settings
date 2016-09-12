24a20a55629abd3ca4be86628e57076592215851
Steve Kondik <steve@cyngn.com>
2016-09-12 01:29:07 -0700

    settings: Fix viewpager tabs not showing on AppOpsSummary
    Change-Id: I311c61559177ea733809f510f017ef613915f3d4
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/applications/AppOpsSummary.java
+++ src/com/android/settings/applications/AppOpsSummary.java
@@ -115,6 +115,9 @@ public class AppOpsSummary extends InstrumentedFragment {
         mViewPager.setOnPageChangeListener(adapter);
         PagerTabStrip tabs = (PagerTabStrip) rootView.findViewById(R.id.tabs);
 
+        // HACK - https://code.google.com/p/android/issues/detail?id=213359
+        ((ViewPager.LayoutParams)tabs.getLayoutParams()).isDecor = true;
+
         Resources.Theme theme = tabs.getContext().getTheme();
         TypedValue typedValue = new TypedValue();
         theme.resolveAttribute(android.R.attr.colorAccent, typedValue, true);
