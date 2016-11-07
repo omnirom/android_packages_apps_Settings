e85e581537f2db1062f8b6d1f364d202c34fb57a
darkobas <darkobas@gmail.com>
2016-11-07 14:02:24 +0100

    Settings: add DeviceParts to OmniGears Settings
    Change-Id: I90a14c9b1e4c2f96c4ba2f90bfbeadef9804d016

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -166,5 +166,6 @@ public class Settings extends SettingsActivity {
 
     public static class SuperSUActivity extends SettingsActivity { /* empty */ }
     public static class SuperUserActivity extends SettingsActivity { /* empty */ }
+    public static class DevicePartsActivity extends SettingsActivity { /* empty */ }
 
 }
