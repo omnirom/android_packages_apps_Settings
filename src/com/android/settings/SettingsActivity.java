92156ad1615a99e187b4b96fdabd4931a787c18e
maxwen <max.weninger@gmail.com>
2017-01-19 03:36:53 +0100

    Settings: dashboard tile layout config
    add config to hide summary and use 2 columns
    always show one more column in landscape mode
    
    Change-Id: Ibb537cc254233a36a20da029a0070d2ff2465fc1

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -236,6 +236,10 @@ public class SettingsActivity extends SettingsDrawerActivity
     private static final String SUPERUSER_FRAGMENT = "com.android.settings.SuperUser";
     private static final String DEVICE_PARTS_FRAGMENT = "org.omnirom.device.DeviceParts";
 
+    public static final String KEY_HIDE_SUMMARY = "hide_summary";
+    public static final String KEY_COLUMNS_COUNT = "columns_count";
+    public static final String APP_PREFERENCES_NAME = "app_settings";
+
     private String mFragmentClass;
 
     private CharSequence mInitialTitle;
@@ -382,6 +386,7 @@ public class SettingsActivity extends SettingsDrawerActivity
 
     private SharedPreferences mDevelopmentPreferences;
     private SharedPreferences.OnSharedPreferenceChangeListener mDevelopmentPreferencesListener;
+    private SharedPreferences mAppPreferences;
 
     private boolean mBatteryPresent = true;
     private BroadcastReceiver mBatteryInfoReceiver = new BroadcastReceiver() {
@@ -551,6 +556,8 @@ public class SettingsActivity extends SettingsDrawerActivity
         }
 
         mDevelopmentPreferences = getSharedPreferences(DevelopmentSettings.PREF_FILE,
+                Context.MODE_PRIVATE);
+        mAppPreferences = getSharedPreferences(APP_PREFERENCES_NAME,
                 Context.MODE_PRIVATE);
 
         // Getting Intent properties can only be done after the super.onCreate(...)
@@ -1352,5 +1359,39 @@ public class SettingsActivity extends SettingsDrawerActivity
                     PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.columns_menu:
+                int columnsCount = mAppPreferences.getInt(KEY_COLUMNS_COUNT, 1);
+                if (columnsCount == 1) {
+                    mAppPreferences.edit().putInt(KEY_COLUMNS_COUNT, 2).commit();
+                } else {
+                    mAppPreferences.edit().putInt(KEY_COLUMNS_COUNT, 1).commit();
+                }
+                return true;
+            case R.id.hide_summary_menu:
+                boolean hideSummary = mAppPreferences.getBoolean(KEY_HIDE_SUMMARY, false);
+                mAppPreferences.edit().putBoolean(KEY_HIDE_SUMMARY, !hideSummary).commit();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem columnMenuItem = menu.findItem(R.id.columns_menu);
+        if (columnMenuItem != null) {
+            int columnsCount = mAppPreferences.getInt(KEY_COLUMNS_COUNT, 1);
+            columnMenuItem.setChecked(columnsCount != 1);
+        }
+        MenuItem hideSummaryMenu = menu.findItem(R.id.hide_summary_menu);
+        if (hideSummaryMenu != null) {
+            boolean hideSummary = mAppPreferences.getBoolean(KEY_HIDE_SUMMARY, false);
+            hideSummaryMenu.setChecked(hideSummary);
+        }
+        return true;
     }
 }
