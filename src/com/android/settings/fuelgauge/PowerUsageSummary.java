67e7d1543155cd9ac75d83b370589f8c02df3cec
maxwen <max.weninger@gmail.com>
2017-10-03 14:33:27 +0200

    [2/2] Settings: poor man battery style tweaks
    for now just add hide/show image and or percentage
    
    Change-Id: I9c5f0bdb79aa007ad9cc779bf2b460b2a89ca4db

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -57,6 +57,7 @@ import com.android.settings.core.instrumentation.MetricsFeatureProvider;
 import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.display.AutoBrightnessPreferenceController;
 import com.android.settings.display.BatteryPercentagePreferenceController;
+import com.android.settings.display.BatteryImagePreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -196,6 +197,7 @@ public class PowerUsageSummary extends PowerUsageBase {
         controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
         controllers.add(new BatterySaverController(context, getLifecycle()));
         controllers.add(new BatteryPercentagePreferenceController(context));
+        controllers.add(new BatteryImagePreferenceController(context));
         return controllers;
     }
 
