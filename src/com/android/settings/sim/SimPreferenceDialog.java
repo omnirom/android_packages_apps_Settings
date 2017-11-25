ed5d55986b6eba3df5ab89a0cf32c4cc0159ad19
maxwen <max.weninger@gmail.com>
2017-11-25 02:51:56 +0100

    Settings: refine the SIM settings preference
    show carrier with card
    use same layout everywhere
    
    Change-Id: Ic260649185a636360feaee937b451f5aa87e443b

--- src/com/android/settings/sim/SimPreferenceDialog.java
+++ src/com/android/settings/sim/SimPreferenceDialog.java
@@ -142,10 +142,9 @@ public class SimPreferenceDialog extends Activity {
             numberView.setText(PhoneNumberUtils.formatNumber(rawNumber));
         }
 
-        String simCarrierName = tm.getSimOperatorName(mSubInfoRecord.getSubscriptionId());
+        CharSequence simCarrierName = getSubscriptionCarrierName(mSubInfoRecord);
         TextView carrierView = (TextView)mDialogLayout.findViewById(R.id.carrier);
-        carrierView.setText(!TextUtils.isEmpty(simCarrierName) ? simCarrierName :
-                mContext.getString(com.android.internal.R.string.unknownName));
+        carrierView.setText(simCarrierName);
 
         mBuilder.setTitle(String.format(res.getString(R.string.sim_editor_title),
                 (mSubInfoRecord.getSimSlotIndex() + 1)));
@@ -180,6 +179,12 @@ public class SimPreferenceDialog extends Activity {
         });
 
         mBuilder.create().show();
+    }
+
+    private String getSubscriptionCarrierName(SubscriptionInfo sir) {
+        CharSequence simCarrierName = sir.getCarrierName();
+        return !TextUtils.isEmpty(simCarrierName) ? simCarrierName.toString() :
+                mContext.getString(com.android.internal.R.string.unknownName);
     }
 
     private class SelectColorAdapter extends ArrayAdapter<CharSequence> {
