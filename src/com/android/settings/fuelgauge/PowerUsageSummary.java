7f8f703e42423041d98593c0c2afb9066b1855b5
maxwen <max.weninger@gmail.com>
2017-12-10 23:50:44 +0100

    Settings: add back missing controller for battery image setting
    Change-Id: I5773a87381ae9987f468eccbbe47f56f78abf69b

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -313,6 +313,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
         //controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
         controllers.add(new BatterySaverController(context, getLifecycle()));
         controllers.add(new BatteryPercentagePreferenceController(context));
+        controllers.add(new BatteryImagePreferenceController(context));
         controllers.add(new AmbientDisplayPreferenceController(
                 context,
                 new AmbientDisplayConfiguration(context),
