b11306a7b847599dffb9d162bb43ffa205211660
maxwen <max.weninger@gmail.com>
2018-03-09 02:10:37 +0100

    [1/3] Settings: settings search index fixes
    for propper creating we must provide our dashboard entries
    directly in the xml file and not via manifest contribution
    
    Change-Id: If81791822433245891e0d27a7aeb59866a930a44

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -202,5 +202,7 @@ public class Settings extends SettingsActivity {
     public static class DisplayRotationActivity extends SettingsActivity { /* empty */ }
     public static class BatteryLightSettingsActivity extends SettingsActivity { /* empty */ }
     public static class OmniJawsSettingsActivity extends SettingsActivity { /* empty */ }
-
+    public static class NetworkTrafficSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class BatteryClockSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class BatteryStyleSettingsActivity extends SettingsActivity { /* empty */ }
 }
