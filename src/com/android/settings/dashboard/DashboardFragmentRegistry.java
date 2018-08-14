0d3760913172b97f71e805f62678032fdd49cedc
maxwen <max.weninger@gmail.com>
2018-08-14 21:22:09 +0200

    [2/2] Settings: bring back OmniGears
    Change-Id: I093e28047919ec31273d690ed286e592ec1a1567

--- src/com/android/settings/dashboard/DashboardFragmentRegistry.java
+++ src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -45,6 +45,8 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import org.omnirom.omnigears.OmniDashboardFragment;
+
 import java.util.Map;
 
 /**
@@ -116,7 +118,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_MY_DEVICE_INFO);
         PARENT_TO_CATEGORY_KEY_MAP.put(BatterySaverSettings.class.getName(),
                 CategoryKey.CATEGORY_BATTERY_SAVER_SETTINGS);
-
+        PARENT_TO_CATEGORY_KEY_MAP.put(OmniDashboardFragment.class.getName(),
+                OmniDashboardFragment.CATEGORY_OMNI);
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 
         for (Map.Entry<String, String> parentToKey : PARENT_TO_CATEGORY_KEY_MAP.entrySet()) {
