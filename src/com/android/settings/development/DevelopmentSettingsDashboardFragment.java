d1c85efd960561f42c5fce3de23a46f1ed348ad5
maxwen <max.weninger@gmail.com>
2018-08-19 22:18:46 +0200

    [2/3] Settings: add support for adb over network
    Change-Id: I09afbfbd0b2794bcaf4addcb7de31d66a409d1b0

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -62,7 +62,8 @@ import java.util.List;
 public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFragment
         implements SwitchBar.OnSwitchChangeListener, OemUnlockDialogHost, AdbDialogHost,
         AdbClearKeysDialogHost, LogPersistDialogHost,
-        BluetoothA2dpHwOffloadRebootDialog.OnA2dpHwDialogConfirmedListener {
+        BluetoothA2dpHwOffloadRebootDialog.OnA2dpHwDialogConfirmedListener,
+        AdbNetworkDialogHost {
 
     private static final String TAG = "DevSettingsDashboard";
 
@@ -293,6 +294,21 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
     }
 
     @Override
+    public void onEnableAdbNetworkDialogConfirmed() {
+        final AdbNetworkPreferenceController controller = getDevelopmentOptionsController(
+                AdbNetworkPreferenceController.class);
+        controller.onAdbDialogConfirmed();
+
+    }
+
+    @Override
+    public void onEnableAdbNetworkDialogDismissed() {
+        final AdbNetworkPreferenceController controller = getDevelopmentOptionsController(
+                AdbNetworkPreferenceController.class);
+        controller.onAdbDialogDismissed();
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         boolean handledResult = false;
         for (AbstractPreferenceController controller : mPreferenceControllers) {
@@ -496,6 +512,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new AutofillResetOptionsPreferenceController(context));
         controllers.add(new TrustAgentsExtendUnlockPreferenceController(context));
         controllers.add(new TrustLostLocksScreenPreferenceController(context));
+        controllers.add(new AdbNetworkPreferenceController(context, fragment));
         return controllers;
     }
 
