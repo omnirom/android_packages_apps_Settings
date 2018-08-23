4ecd6b5bc415f1f58fd6e55721071542a539f235
xyyx <xyyx@mail.ru>
2018-08-23 21:22:48 +0800

    Settings: Allow to customize notification led light
    Reworked with Controllers for P
    Thanks @maxwen and @ezio84 for old patches in O
    
    Change-Id: I95a72675449ed21a136b410e8c4bc4ac6debe2ee

--- src/com/android/settings/notification/AppNotificationSettings.java
+++ src/com/android/settings/notification/AppNotificationSettings.java
@@ -125,6 +125,24 @@ public class AppNotificationSettings extends NotificationSettingsBase {
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mNm.forcePulseLedLight(-1, -1, -1);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mNm.forcePulseLedLight(-1, -1, -1);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mNm.forcePulseLedLight(-1, -1, -1);
+    }
+
+    @Override
     protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
         mControllers = new ArrayList<>();
         mControllers.add(new HeaderPreferenceController(context, this));
@@ -137,6 +155,10 @@ public class AppNotificationSettings extends NotificationSettingsBase {
         mControllers.add(new SoundPreferenceController(context, this,
                 mImportanceListener, mBackend));
         mControllers.add(new LightsPreferenceController(context, mBackend));
+        mControllers.add(new CustomLightsPreferenceController(context, mBackend));
+        mControllers.add(new CustomLightOnTimePreferenceController(context, mBackend));
+        mControllers.add(new CustomLightOffTimePreferenceController(context, mBackend));
+        mControllers.add(new LightOnZenPreferenceController(context, mBackend));
         mControllers.add(new VibrationPreferenceController(context, mBackend));
         mControllers.add(new VisibilityPreferenceController(context, new LockPatternUtils(context),
                 mBackend));
