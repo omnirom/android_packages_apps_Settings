5bd2ff86c3acb9799a907858c40f2fdd9e516b03
Marko Man <darkobas@gmail.com>
2018-08-27 15:32:28 +0200

    Settings: Enable BatteryLight from OmniGears
    Change-Id: Id28d77afc34b3da2cba1c2ff234e5c0498682108

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -141,6 +141,7 @@ import org.omnirom.omnigears.OmniDashboardFragment;
 import org.omnirom.omnigears.ButtonSettings;
 import org.omnirom.omnigears.GestureSettings;
 import org.omnirom.omnigears.interfacesettings.BarsSettings;
+import org.omnirom.omnigears.batterylight.BatteryLightSettings;
 import org.omnirom.omnigears.interfacesettings.StyleSettings;
 import org.omnirom.omnigears.moresettings.MoreSettings;
 
@@ -276,6 +277,7 @@ public class SettingsGateway {
             MoreSettings.class.getName(),
             GestureSettings.class.getName(),
             BarsSettings.class.getName(),
+            BatteryLightSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
