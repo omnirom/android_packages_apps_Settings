f07c6abb97aa543d6e65ed10bc74f71a8c088e5a
Ting Yu <ting.x.yu@sony.com>
2018-10-18 16:56:22 +0800

    Settings: Phone ringtone setting for Multi SIM device [2/3]
    Support displaying phone ringtone setting for each slot as follows:
    "Phone ringtone - SIM 1"
    "Phone ringtone - SIM 2"
    
    The purpose is to distinguish incoming call from each slot by ringtone.
    
    Depends-On: I9c6ccff938122332d222853d469ad9a623c7d193
    
    Bug: 118735436
    Test: Manual
    Change-Id: I06c59016e9b6da51ed8a4678a66674fc48c20e40

--- src/com/android/settings/notification/RingtonePreferenceControllerBase.java
+++ src/com/android/settings/notification/RingtonePreferenceControllerBase.java
@@ -24,6 +24,7 @@ import android.util.Log;
 
 import androidx.preference.Preference;
 
+import com.android.settings.RingtonePreference;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.utils.ThreadUtils;
@@ -51,8 +52,8 @@ public abstract class RingtonePreferenceControllerBase extends AbstractPreferenc
     }
 
     private void updateSummary(Preference preference) {
-        final Uri ringtoneUri = RingtoneManager.getActualDefaultRingtoneUri(
-                mContext, getRingtoneType());
+        final Uri ringtoneUri = RingtoneManager.getActualDefaultRingtoneUriBySlot(mContext,
+                getRingtoneType(), ((RingtonePreference)preference).getSlotId());
 
         final CharSequence summary;
         try {
