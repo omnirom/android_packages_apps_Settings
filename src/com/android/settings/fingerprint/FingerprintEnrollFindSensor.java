e509bc4fb6267df1d6785c459bfbc8aaadb74bb2
micky387 <mickaelsaibi@free.fr>
2019-01-09 19:41:15 +0100

    Settings: Add enroll for fingerprint sensor location under the screen
    4 = under the screen
    
    Change-Id: I3e071574ac3ee96472cf6d087322ab8ea6d5a5cc

--- src/com/android/settings/fingerprint/FingerprintEnrollFindSensor.java
+++ src/com/android/settings/fingerprint/FingerprintEnrollFindSensor.java
@@ -42,6 +42,7 @@ public class FingerprintEnrollFindSensor extends FingerprintEnrollBase {
     private static final int SENSOR_LOCATION_FRONT = 1;
     private static final int SENSOR_LOCATION_LEFT = 2;
     private static final int SENSOR_LOCATION_RIGHT = 3;
+    private static final int SENSOR_LOCATION_UNDERSCREEN = 4;
     public static final String EXTRA_KEY_LAUNCHED_CONFIRM = "launched_confirm_lock";
 
     @Nullable
@@ -76,7 +77,7 @@ public class FingerprintEnrollFindSensor extends FingerprintEnrollBase {
         }
 
         int sensorLocation = getResources().getInteger(R.integer.config_fingerprintSensorLocation);
-        if (sensorLocation < SENSOR_LOCATION_BACK || sensorLocation > SENSOR_LOCATION_RIGHT) {
+        if (sensorLocation < SENSOR_LOCATION_BACK || sensorLocation > SENSOR_LOCATION_UNDERSCREEN) {
             sensorLocation = SENSOR_LOCATION_BACK;
         }
         final String location = getResources().getStringArray(
@@ -87,6 +88,11 @@ public class FingerprintEnrollFindSensor extends FingerprintEnrollBase {
                 location));
         if (sensorLocation == SENSOR_LOCATION_FRONT) {
             findViewById(R.id.fingerprint_sensor_location_front_overlay)
+                    .setVisibility(View.VISIBLE);
+        }
+
+        if (sensorLocation == SENSOR_LOCATION_UNDERSCREEN) {
+            findViewById(R.id.fingerprint_sensor_location)
                     .setVisibility(View.VISIBLE);
         }
     }
