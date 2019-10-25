adc4d9e3589ab9ab283749f3a8f754ac4c2427ba
althafvly <althafvly@gmail.com>
2019-10-25 07:16:45 +0000

    Settings: Protect sensitive data on the about phone
    - Hide some personal info like phone number and IMEI.
    - IMEI shows on a tap, so why not.
    - Avoid showing this in screenshot/screenrecord
    
    Co-authored-by: El Dainosor <eldainosor@gmail.com>
    Change-Id: I45b7898da440c36045aec64a0a86fb804116cd9d

--- src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
+++ src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
@@ -100,7 +100,7 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
 
     @Override
     public CharSequence getSummary() {
-        return getSummary(0);
+        return mContext.getString(R.string.device_info_protected_single_press);
     }
 
     private CharSequence getSummary(int simSlot) {
@@ -117,6 +117,7 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
         }
 
         ImeiInfoDialogFragment.show(mFragment, simSlot, preference.getTitle().toString());
+        preference.setSummary(getSummary(simSlot));
         return true;
     }
 
@@ -134,7 +135,7 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
 
     private void updatePreference(Preference preference, int simSlot) {
         preference.setTitle(getTitle(simSlot));
-        preference.setSummary(getSummary(simSlot));
+        preference.setSummary(getSummary());
     }
 
     private CharSequence getTitleForGsmPhone(int simSlot) {
