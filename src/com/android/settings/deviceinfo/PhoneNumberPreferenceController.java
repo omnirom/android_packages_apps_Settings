0a0b870cb9c32ce4d85343ca666b4ec679b0c5fb
Marko Man <darkobas@gmail.com>
2020-05-02 02:46:26 +0200

    Settings: fix sensitive info handling in About phone
    Change-Id: Ibc465421d7fc66ebde76d7da70ab58628a3d2e01

--- src/com/android/settings/deviceinfo/PhoneNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/PhoneNumberPreferenceController.java
@@ -60,7 +60,7 @@ public class PhoneNumberPreferenceController extends BasePreferenceController {
 
     @Override
     public CharSequence getSummary() {
-        if (mContext.getResources().getBoolean(R.bool.configShowDeviceSensitiveInfo) && mTapped) {
+        if (mContext.getResources().getBoolean(R.bool.configShowDeviceSensitiveInfo) || mTapped) {
             return getFirstPhoneNumber();
         }
         return mContext.getString(R.string.device_info_protected_single_press);
@@ -105,9 +105,10 @@ public class PhoneNumberPreferenceController extends BasePreferenceController {
         if (simSlotNumber == -1) {
             return false;
         }
-        mTapped = true;
+        mTapped = !mTapped;
         final Preference simStatusPreference = mPreferenceList.get(simSlotNumber);
         simStatusPreference.setSummary(getPhoneNumber(simSlotNumber));
+        updateState(simStatusPreference);
         return true;
     }
 
