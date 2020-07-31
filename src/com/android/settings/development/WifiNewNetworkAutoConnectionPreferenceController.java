1406101fd6650c34c6aaf60100e15ee50066d9e3
Veerendranath Jakkam <vjakkam@codeaurora.org>
2020-07-31 13:43:01 +0530

    wifi: Add developer options for 2GHz and 5GHz SSID Bridging feature
    This change adds two developer options to control SSID Bridging feature
    - Option to enable/disable Fast handover between different SSIDs
    - Option to enable/disable Auto connection attempts to a new 5G SSID
    
    Change-Id: Iec170a9bb908716705c94682e1c77cfb34113834
    CRs-Fixed: 2746291

---
+++ src/com/android/settings/development/WifiNewNetworkAutoConnectionPreferenceController.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of The Linux Foundation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.android.settings.development;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import androidx.annotation.VisibleForTesting;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.development.DeveloperOptionsPreferenceController;
+
+public class WifiNewNetworkAutoConnectionPreferenceController extends DeveloperOptionsPreferenceController
+        implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {
+
+    private static final String WIFI_NEW_NETWORK_AUTO_CONNECTION_KEY = "wifi_new_network_auto_connection";
+
+    private final WifiManager mWifiManager;
+
+    public WifiNewNetworkAutoConnectionPreferenceController(Context context) {
+        super(context);
+
+        mWifiManager = context.getSystemService(WifiManager.class);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return WIFI_NEW_NETWORK_AUTO_CONNECTION_KEY;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final boolean isEnabled = (Boolean) newValue;
+        mWifiManager.enableNewNetworkAutoConnection(isEnabled);
+        return true;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        final boolean enabled = mWifiManager.isNewNetworkAutoConnectionEnabled();
+        ((SwitchPreference) mPreference).setChecked(enabled);
+
+    }
+
+    @Override
+    protected void onDeveloperOptionsSwitchDisabled() {
+        super.onDeveloperOptionsSwitchDisabled();
+        mWifiManager.enableNewNetworkAutoConnection(false);
+        ((SwitchPreference) mPreference).setChecked(false);
+    }
+}
+
