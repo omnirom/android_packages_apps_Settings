1b61972eeaff5d70dd9a4dbd13b73575082437ab
maxwen <max.weninger@gmail.com>
2020-10-06 01:14:34 +0200

    [4/4] Settings: bring back all changes from Build.FINGERPRINT to Build.TIME
    use Build.TIME to check for slice db update
    
    Change-Id: Id0de43de5deb12b21dd410941dbe8a954640c109

--- src/com/android/settings/slices/SlicesDatabaseHelper.java
+++ src/com/android/settings/slices/SlicesDatabaseHelper.java
@@ -246,6 +246,6 @@ public class SlicesDatabaseHelper extends SQLiteOpenHelper {
 
     @VisibleForTesting
     String getBuildTag() {
-        return Build.FINGERPRINT;
+        return String.valueOf(Build.TIME);
     }
 }
