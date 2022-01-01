c088a1ec25bc92975344a6806b7a72b579756f27
maxwen <max.weninger@gmail.com>
2022-01-01 10:46:18 +0100

    Settings: add more controllers to calc gesture visibility
    Change-Id: I6c7e1357a59fdfd524e867194a47fd35826a3d83

--- src/com/android/settings/gestures/GesturesSettingPreferenceController.java
+++ src/com/android/settings/gestures/GesturesSettingPreferenceController.java
@@ -77,6 +77,9 @@ public class GesturesSettingPreferenceController extends BasePreferenceControlle
         controllers.add(new DoubleTapScreenPreferenceController(context, FAKE_PREF_KEY)
                 .setConfig(ambientDisplayConfiguration));
         controllers.add(new PreventRingingParentPreferenceController(context, FAKE_PREF_KEY));
+        controllers.add(new PowerMenuPreferenceController(context, FAKE_PREF_KEY));
+        controllers.add(new OneHandedPreferenceCategoryController(context, FAKE_PREF_KEY));
+        controllers.add(new SystemNavigationPreferenceController(context, FAKE_PREF_KEY));
         return controllers;
     }
 }
