7239a516a09d4098be1ddbad25cca76bf1dcabc2
maxwen <max.weninger@gmail.com>
2022-01-29 13:56:06 +0100

    Settings: check wifi direct feature for showing preferences
    Change-Id: I706bd7bb35e8da004a0dfc8c0949beea082514ea

--- src/com/android/settings/wifi/p2p/WifiP2pPreferenceController.java
+++ src/com/android/settings/wifi/p2p/WifiP2pPreferenceController.java
@@ -15,6 +15,8 @@
  */
 package com.android.settings.wifi.p2p;
 
+import static android.content.pm.PackageManager.FEATURE_WIFI_DIRECT;
+
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -101,8 +103,7 @@ public class WifiP2pPreferenceController extends AbstractPreferenceController
 
     @Override
     public boolean isAvailable() {
-        // Always show preference.
-        return true;
+        return mContext.getPackageManager().hasSystemFeature(FEATURE_WIFI_DIRECT);
     }
     @Override
     public String getPreferenceKey() {
