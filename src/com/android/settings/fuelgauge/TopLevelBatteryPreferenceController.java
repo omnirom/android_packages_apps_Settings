b83acf238d946530b307f39b02f56b1c004c86d9
maxwen <max.weninger@gmail.com>
2022-04-26 21:22:24 +0200

    Settings: use mIsBatteryPresent for visibility of top level battery setting
    Change-Id: I659000983e7421ae0b13332a589d9699f6dc23f3

--- src/com/android/settings/fuelgauge/TopLevelBatteryPreferenceController.java
+++ src/com/android/settings/fuelgauge/TopLevelBatteryPreferenceController.java
@@ -67,6 +67,8 @@ public class TopLevelBatteryPreferenceController extends BasePreferenceControlle
                                 setSummaryAsync(info);
                             },
                             true /* shortString */);
+                            // mIsBatteryPresent could change
+                            mPreference.setVisible(getAvailabilityStatus() == AVAILABLE);
                 });
 
         mBatteryStatusFeatureProvider =
@@ -75,7 +77,7 @@ public class TopLevelBatteryPreferenceController extends BasePreferenceControlle
 
     @Override
     public int getAvailabilityStatus() {
-        return mContext.getResources().getBoolean(R.bool.config_show_top_level_battery)
+        return mContext.getResources().getBoolean(R.bool.config_show_top_level_battery) && mIsBatteryPresent
                 ? AVAILABLE
                 : UNSUPPORTED_ON_DEVICE;
     }
