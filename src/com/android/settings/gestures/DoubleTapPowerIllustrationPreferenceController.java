1bb514f61f4c01c266998389d5b89cd852330fcd
micky387 <mickaelsaibi@free.fr>
2025-12-12 17:37:43 -0500

    Revert "Settings: Pass empty lottie resource for quickly open camera animation"
    This reverts commit 4e62a1ead63d95bce6bd0bbbdc899690810c7e42.
    
    Change-Id: I88ac209d4e795a44aa2cac2a6566decd910549d4

--- src/com/android/settings/gestures/DoubleTapPowerIllustrationPreferenceController.java
+++ src/com/android/settings/gestures/DoubleTapPowerIllustrationPreferenceController.java
@@ -78,7 +78,7 @@ public class DoubleTapPowerIllustrationPreferenceController extends BasePreferen
                         DoubleTapPowerSettingsUtils
                                 .isDoubleTapPowerButtonGestureForCameraLaunchEnabled(
                                         mContext)
-                                ? R.raw.lottie_quickly_open_camera
+                                ? R.drawable.quickly_open_camera
                                 : R.drawable.double_tap_power_for_wallet);
     }
 
